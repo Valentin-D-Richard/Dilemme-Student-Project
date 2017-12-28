8dd4bec3ea564a2de99c091a32ce6118949cb2bf
Valrictin <34923985+Valrictin@users.noreply.github.com>
2017-12-28 23:45:00 +0100

    Create dilemme1.c

---
+++ dilemme1.c
@@ -0,0 +1,13 @@
+#include <dilemmelib.h>
+
+bool** affr(void strat1(bool**,int,int), void strat2(bool**,int,int), int n) {
+  bool** g = malloc(n*sizeof(bool*));
+  for (int i; i < n; i++) { strat1(g,i,0) ; strat2(g,i,1) }
+  return(g);
+}
+
+int main (int argc, char** argv) {
+  bool** g = affr(gentille,gentille,1) ;
+  printf("Gentille vs Gentille : %b à %b",g[0][0],g[0][1]);
+  return(0);
+  }
