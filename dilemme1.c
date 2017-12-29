3e6a1bd4dbe2b6d3a8d0b429836b46ab32f77932
Valrictin <34923985+Valrictin@users.noreply.github.com>
2017-12-30 00:07:30 +0100

    Update dilemme1.c

--- dilemme1.c
+++ dilemme1.c
@@ -1,15 +1,27 @@
 #include "dilemmelib.h"
 
-bool** affr(void strat1(bool**,int,int), void strat2(bool**,int,int), int n) {
-  bool** g = malloc(n*sizeof(bool*));
-  for (int i; i < n; i++) { 
-    g[i] = malloc(2*sizeof(bool));
-    strat1(g,i,0) ; strat2(g,i,1); }
-  return(g);
+void affr(void strat1(int**,int,int), void strat2(int**,int,int), int** g, int n) {
+  for (int i; i < n; i++) { strat1(g,i,0) ; strat2(g,i,1); }
 }
 
 int main (int argc, char** argv) {
-  bool** g = affr(gentille,gentille,1) ;
-  printf("Gentille vs Gentille : %d à %d",g[0][0],g[0][1]);
-  return(0);
+  init_dico();
+  if (argc != 4) { printf("usage : <strat1> <strat2> <nb iterations>\n"); return(-1); }
+  else {
+    int i1,i2,n;
+
+    i1 = assoc(argv[1]); i2 = assoc(argv[2]);
+    if (i1 == -1 || i2 == -1) (printf("Wrong name of startegy\n"), exit(-1)) ;
+    n = atoi(argv[3]);
+
+    int** g; g = malloc(n*sizeof(int*));
+    for (int i = 0; i < n; i++) g[i] = malloc(2*sizeof(int)) ;
+    affr(*dico[i1].fun,*dico[i2].fun,g,n) ;
+    printf("%s vs %s : %d à %d\n",dico[i1].name,dico[i2].name,g[n-1][0],g[n-1][1]);
+
+    int win[2];
+    gain(g[n-1],win);
+    printf("%s vs %s : gain %d à %d\n",dico[i1].name,dico[i2].name,win[0],win[1]);
+    return(0);
   }
+}
