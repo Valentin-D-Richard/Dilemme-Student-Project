75a5f477ca7dd11843252ecadc9b2b966f155a3d
Valrictin <34923985+Valrictin@users.noreply.github.com>
2017-12-29 14:41:56 +0100

    Update dilemme1.c

--- dilemme1.c
+++ dilemme1.c
@@ -2,12 +2,12 @@
 
 bool** affr(void strat1(bool**,int,int), void strat2(bool**,int,int), int n) {
   bool** g = malloc(n*sizeof(bool*));
-  for (int i; i < n; i++) { strat1(g,i,0) ; strat2(g,i,1) }
+  for (int i; i < n; i++) { strat1(g,i,0) ; strat2(g,i,1); }
   return(g);
 }
 
 int main (int argc, char** argv) {
   bool** g = affr(gentille,gentille,1) ;
-  printf("Gentille vs Gentille : %b à %b",g[0][0],g[0][1]);
+  printf("Gentille vs Gentille : %d à %d",g[0][0],g[0][1]);
   return(0);
   }
