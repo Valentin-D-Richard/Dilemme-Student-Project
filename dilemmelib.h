1f4804e4c14b123edc32a9bb9d47b8428bc65c12
Yaugzebul <34932839+Yaugzebul@users.noreply.github.com>
2017-12-30 07:44:27 +0100

    Update dilemmelib.h

--- dilemmelib.h
+++ dilemmelib.h
@@ -42,11 +42,30 @@ void mefiante (int** g, int i, int r) {
   (i == 0) ? (g[0][r] = 0) : (g[i][r] = g[i-1][1-r]) ;
 }
 
+void periodique_mechante (int** g, int i, int r){
+	(i%3==2) ? (g[i][r]=1) : (g[i][r]=0);
+}
+
+void periodique_gentille (int** g, int i, int r){
+	(i%3==2) ? (g[i][r]=0) : (g[i][r]=1);
+}
+
+void majorite_mou (int** g, int i, int r){
+	(i==0) ? (g[i][r] = 1):
+	int s =maj(g,1-r);
+	(s>=1) ? (g[i][r] = 1) : (g[i][r] = 0);
+}
+
+void majorite_dur (int** g, int i, int r){
+	(i==0) ? (g[i][r] = 0):
+	int s =maj(g,1-r);
+	(s>1) ? (g[i][r] = 1) : (g[i][r] = 0);
+}
 // ***** Fonctions générales *****
 
 typedef struct dictionnaire { char* name ; void (*fun)(int**,int,int); } dictionnaire ;
 
-#define N 4 // Nombre de stratégies
+#define N 6 // Nombre de stratégies
 dictionnaire dico[N]; // Dictionnaire d'associations
 
 void init_dico() {
@@ -54,6 +73,9 @@ void init_dico() {
   dico[1].name = "mechante" ; dico[1].fun = &mechante ;
   dico[2].name = "donnant_donnant" ; dico[2].fun = &donnant_donnant ;
   dico[3].name = "mefiante" ; dico[3].fun = &mefiante ;
+  dico[4].name = "periodique_mechante" ; dico[4].fun = &periodique_mechante ;
+dico[5].name = "periodique_gentille" ; dico[5].fun = &periodique_gentille ;
+
 }
 
 int assoc(char* nom) {
@@ -62,3 +84,13 @@ int assoc(char* nom) {
   for (int i = 0; i < N; i++) if (!strcmp(nom,dico[i].name)) return(i) ;
   return(-1);
 }
+
+//calcul la majorité dans un tableau jusqu'à l'indice i on suppose i<=taille tableau et r la ligne qu'on regarde
+ int maj(int** g, int r){
+	 int s
+for (int j=0,j<(i-1),j++){
+	s=s+g[j][r];
+}
+(s==(i/2)) ? (return 2) : (s>(i/2)) ? (return 1) : (return 0);
+ }
+	
