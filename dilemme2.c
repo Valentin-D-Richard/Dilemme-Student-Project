596cbf73df3106739655082ad31cdc59634cf1ef
Yaugzebul <34932839+Yaugzebul@users.noreply.github.com>
2018-01-08 21:12:51 +0100

    Update dilemme2.c

--- dilemme2.c
+++ dilemme2.c
@@ -51,7 +51,64 @@ int** ecosysteme ( int n, int p, int nb){
  return c;
 }
    
-    
+// il y a une deuxième façon de voir la question  qui est de faire à chaque fois une seule confrontation    
+
+int** ecosysteme2 ( int n, int p){
+ init_dico();
+ int g[11][11][n];
+  // c c'est le tableaux avec les valeurs des nombres d'individus
+ int c[11][n];
+ for (int i=0;i<11;i++){
+  c[i][0]=p;
+ }
+ for (int i=0;i<n,i++){
+  for (int j=0;j<11;j++) {
+   for (int k=0;k<=j;k++){
+    int t[n][2];
+    dico[j].fun(t,i,0);
+    dico[k].fun(t,i,1);
+    g[j][k][i]=t[i][0];
+    g[k][j][i]=t[i][1];
+   }
+  }
+  int P[11];
+  int r[11][11];
+  for (int k;k<11;k++){
+   for (int j;j<11;j++){
+    int p[2]=[g[k][j][i],g[j][k][i]];
+    int t[2];
+    gain(p,t);
+    r[k][j]=t[0];
+    r[j][k]=t[1];
+   }
+  }
+  for (int j=0;j<11;j++){
+   for (int k=0;k<11;k++){
+    (k==j) ? (P[j]+=(c[j][i-1]-1)*r[j][j]) : (P[j]+=(c[j][i-1])*r[j][k]) ;
+   }
+  }
+  for (int j=0;j<11;j++){
+   P[j]*=c[j][i-1];
+  }
+  int s1=0;
+  for (int j=0;j<11:j++){
+   s1+=P[j];
+  }
+  int s2=0;
+  //je vais remplir maintenant la i-ème génération avec des parties entières mais pour ne rien perde pour la dernière
+  //stratégie en va faire effectif total - les autres effectifs
+  for (int j=0;j<10;j++){
+   c[j][i]=11*p*P[j];
+   c[j][i]/=s1;
+   s2+=c[j][i]; 
+  }
+  c[10][i]=11*p-s2;
+ }
+ return c;
+}
    
+   
+   
+ 
    
    
