c9bda3a0a83b155c77d00caa84a4de609df06687
Valrictin <34923985+Valrictin@users.noreply.github.com>
2018-01-19 17:57:17 +0100

    Update dilemme3.c

--- dilemme3.c
+++ dilemme3.c
@@ -1,96 +1,244 @@
-/* Fichier qui gère la création du réseau et lance les appels ssh puis les clients et le serveur */
-
-#include <pthread.h>
+#include <errno.h>
+#include <time.h>
+#include <ulimit.h>
 #include <unistd.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+#include "packets.h"
 #include "dilemmelib.h"
 
+// maximum number of files that a process can open
+#define MAXFILES (ulimit(4))
+
+// maximal size of a file that we'll allow
+#define MAXSIZE 65535
+
+// name of history file
+#define HISTORY "history.txt"
+
+#define MAXFILES (ulimit(4))
 int NBVILLES;
 
-void* routine (void* data) {
-  char* s = (char*)data;
-  system(s);
-  return(NULL);
+int all_true(int* tab, int n) {
+  for (int i = 0; i < n; i++) {
+    if (!tab[i]) return(0);
+  }
+  return(1);
 }
 
-int main (int argc, char** argv) {
-  if (argc != 5) {printf("usage: <nb_generations> <port> <ip_address> <nb_villes<=23>\n"); exit(-1);}
-  NBVILLES = atoi(argv[4]); if (NBVILLES > 23) {printf("Trop de villes\n"); exit(-1);}
-  int j;
+void init_0(int* tab, int n) { for (int i = 0; i < n; i++) tab[i] = 0; }
 
-  // ****************************** Partie modifiable **********************************
-  // Attention aux espaces !!!
-
-  char** p; p = malloc(NBVILLES*sizeof(char*));
-  for (j = 0; j < NBVILLES; j++) p[j] = malloc(10*sizeof(char));
-  char** pop; pop = malloc(NBVILLES*sizeof(char*));
-  for (j = 0; j < NBVILLES; j++) p[j] = malloc(23*sizeof(char));
-  char** coef; coef = malloc(NBVILLES*sizeof(char*));
-  for (j = 0; j < NBVILLES; j++) p[j] = malloc(30*sizeof(char));
-
-  // p                   // pop                               // coef
-  p[0] = " 100 ";        pop[0] = "1 1 1 1 1 0 0 0 0 1 1 ";   coef[0] = "5 0 3 1 ";   // Ville 1
-  p[1] = " 200 ";        pop[1] = "1 1 1 1 1 1 1 1 1 1 1 ";   coef[1] = "5 0 10 1 ";  // Ville 2
-  p[2] = " 100 ";        pop[2] = "0 1 0 0 1 1 1 0 1 1 1 ";   coef[2] = "5 1 1 1 ";   // Ville 3
-  p[3] = " 500 ";        pop[3] = "1 1 1 1 1 0 1 0 1 0 1 ";   coef[3] = "3 0 5 4 ";   // Ville 4
-  p[4] = " 1000 ";       pop[4] = "1 1 1 1 1 1 1 1 0 1 1 ";   coef[4] = "1 0 3 1 ";   // Ville 5
-  p[5] = " 100 ";        pop[5] = "0 0 0 0 1 1 0 0 0 0 0 ";   coef[5] = "5 0 3 1 ";   // Ville 6
-  p[6] = " 100 ";        pop[6] = "1 1 1 0 1 0 1 0 1 1 1 ";   coef[6] = "5 0 1 3 ";   // Ville 7
-  p[7] = " 100 ";        pop[7] = "0 1 1 1 1 1 0 0 0 1 1 ";   coef[7] = "5 0 3 1 ";   // Ville 8
-  p[8] = " 200 ";        pop[8] = "1 1 1 1 1 0 1 1 0 1 1 ";   coef[8] = "5 2 6 1 ";   // Ville 9
-  p[9] = " 100 ";        pop[9] = "0 1 0 0 1 1 1 0 1 1 1 ";   coef[9] = "5 1 1 10 ";  // Ville 10
-  p[10]= " 500 ";        pop[10]= "1 1 1 1 1 0 1 0 0 0 1 ";   coef[10]= "3 0 5 4 ";   // Ville 11
-  p[11]= " 100 ";        pop[11]= "1 1 1 1 0 1 1 1 0 1 1 ";   coef[11]= "1 2 3 1 ";   // Ville 12
-  p[12]= " 50 ";         pop[12]= "0 0 1 0 1 1 0 0 0 0 0 ";   coef[12]= "6 0 3 1 ";   // Ville 13
-  p[13]= " 100 ";        pop[13]= "1 1 1 0 1 0 0 0 1 1 1 ";   coef[13]= "5 0 8 3 ";   // Ville 14
-  p[14]= " 100 ";        pop[14]= "1 1 0 1 1 0 0 0 1 1 1 ";   coef[14]= "3 0 3 1 ";   // Ville 15
-  p[15]= " 300 ";        pop[15]= "1 1 1 1 1 1 1 1 0 1 1 ";   coef[15]= "5 0 5 1 ";   // Ville 16
-  p[16]= " 100 ";        pop[16]= "0 1 0 0 1 0 1 0 0 0 1 ";   coef[16]= "5 1 1 6 ";   // Ville 17
-  p[17]= " 500 ";        pop[17]= "1 1 0 1 1 0 1 0 1 0 0 ";   coef[17]= "3 0 4 4 ";   // Ville 18
-  p[18]= " 800 ";        pop[18]= "0 1 1 1 1 0 1 1 0 1 1 ";   coef[18]= "1 0 3 8 ";   // Ville 19
-  p[19]= " 100 ";        pop[19]= "1 0 1 0 1 1 0 0 0 0 0 ";   coef[19]= "5 9 3 1 ";   // Ville 20
-  p[20]= " 100 ";        pop[20]= "1 0 1 0 1 0 1 0 0 1 1 ";   coef[20]= "5 0 5 3 ";   // Ville 21
-  p[21]= " 700 ";        pop[21]= "0 1 1 1 1 1 0 1 0 1 1 ";   coef[21]= "5 0 6 1 ";   // Ville 22
-  p[22]= " 200 ";        pop[22]= "1 1 0 1 1 0 1 0 0 1 1 ";   coef[22]= "5 0 6 1 ";   // Ville 23
-
-  /* On rappelle l'ordre des stratégies
-     0  gentille
-     1  mechante
-     2  donnant_donnant
-     3  mefiante
-     4  periodique_gentille
-     5  periodique_mechante
-     6  majorite_moi
-     7  majorite_dur
-     8  sondeur
-     9  donnant_donnant_dur
-     10 rancuniere
-  */
-  // ***********************************************************************************
-
-  // Préparation des arguments
-  char st[NBVILLES][200];
-  char* buff; buff = malloc(2*sizeof(char));
+void print_server (int i, int* corr, int** glb_pop, int** immigr) {
+  int j,i1;
+  printf("\t\t\t\t\t***** Génération %d *****\n",i);
   for (j = 0; j < NBVILLES; j++) {
-    strcpy(st[j],"ssh "); if (j < 9) strcat(st[j],"0"); sprintf(buff,"%d",j+1); strcat(st[j],buff);
-    strcat(st[j],".dptinfo.ens-cachan.fr \"$PWD/client ");
-    strcat(st[j],argv[1]); strcat(st[j],p[j]); strcat(st[j],pop[j]); strcat(st[j],coef[j]);
-    strcat(st[j],argv[2]); strcat(st[j]," "); strcat(st[j],argv[3]); strcat(st[j]," ");
-    strcat(st[j],buff); strcat(st[j],"\"");
+    printf("Ville %d :\nPopulation\t",corr[j]);
+    for (i1 = 0; i1 < N; i1++) printf("%d\t",glb_pop[j][i1]);
+    printf("\nMigrations\t");
+    for (i1 = 0; i1 < N; i1++) printf("%d\t",immigr[j][i1]);
+    printf("\n");
+  }
+}
+
+int get_server_socket (int port)
+{
+	struct sockaddr_in server_address;
+
+	// create a TCP socket
+	int s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (s < 0) fatal_error("No socket available");
+
+	// connect the socket to the chosen port
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	server_address.sin_port = htons(port);
+	if (bind(s,(struct sockaddr *)&server_address,
+				sizeof server_address))
+		fatal_error("Can't bind port");
+
+	// make the socket listen for connections
+	if (listen(s,5)) fatal_error("listen failed");
+
+	return s;
+}
+
+
+int wait_for_client (int socket)
+{
+	while (1)
+	{
+		// let a client connect to the socket
+		int client_socket = accept(socket,NULL,NULL);
+
+		if (client_socket < 0)
+		{
+			if (errno == EINTR || errno == EWOULDBLOCK)
+				continue;
+			else
+				fatal_error("Can't accept connection");
+		}
+
+		return client_socket;
+	}
+}
+
+int binom(int n) {
+  // loi binomiale de pamètres n, 1/3
+  int s = 0;
+  for (int i = 0; i < n; i++) s += (rand() % 3 == 0) ? 1 : 0;
+  return(s);
+}
+
+void migration(int** g, int** immigr) {
+  int p[NBVILLES]; // p[i] contient la population totale de la ville i
+  init_0(p,NBVILLES);
+  int i,i1;
+  for (i = 0; i < NBVILLES; i++) {
+    for (i1 = 0; i1 < N ; i1++) {
+      (g[i][i1] !=-1) ? (p[i] += g[i][i1]) : (i = i) ;
+    }
+  }
+  for (i = 0; i < NBVILLES; i++) init_0(immigr[i],N);
+
+  int c[N]; //c[i1] contient le numero de la ville qui possède la plus grande proportion de strat i1
+  for (i = 0; i < N; i++) c[i]=0;
+  // Cacul de c
+  for (i1 = 0; i1 < N; i1++) {
+    for (i = 0; i < NBVILLES; i++) {
+      ((float)g[i][i1] / (float)p[i] > (float)g[c[i1]][i1] / (float)p[c[i1]]) ? (c[i1] = i) : (i = i);
+    }
   }
 
-  char s[200];
-  strcpy(s,"./server ");
-  strcat(s,argv[1]) ; strcat(s," "); strcat(s,argv[2]); strcat(s," "); strcat(s,argv[4]);
+  // Calcul de immigr
+  for (i = 0; i < NBVILLES; i++) {
+    for (i1 = 0; i1 < N; i1++) {
+      float ratio = (float)g[i][i1] / (float)p[i];
+      if (g[i][i1] != -1 && g[c[i1]][i1] != -1 && ratio <= 0.04) {
+        int migr = binom(g[i][i1]);
+        immigr[c[i1]][i1] += migr;
+        immigr[i][i1] -= migr;
+      };
+    }
+  }
+}
+
+int main (int argc, char **argv)
+{ srand(time(NULL));
+
+  if (argc != 4) {printf("usage: <nb_parties> <port> <nb_villes>\n"); exit(-1);}
+  int i,j;
+  NBVILLES = atoi(argv[3]);
+
+  int n = atoi(argv[1]);
+  int* resp; resp = malloc(NBVILLES*sizeof(int));
+  int** glb_pop; glb_pop = malloc(NBVILLES*sizeof(int*));
+  for (j = 0; j < NBVILLES; j++) {
+    resp[j] = 0; // resp[j] = 1 ssi la ville j a répondu
+    glb_pop[j] = malloc(N*sizeof(int));
+  }
+
+  char *msg;
+  fd_set readfds;
+  int b = 1;
 
 
-  // Lancements parallèles
-  pthread_t* pt; pt = malloc(NBVILLES*sizeof(pthread_t)); pthread_t serv;
-  pthread_create(&serv,NULL,routine,(void*)s);
-  sleep(1);
-  for (j = 0; j < NBVILLES; j++) pthread_create(&pt[j],NULL,routine,(void*)st[j]);
+  // Connexions
+  printf("Connexions\n|");
+  int socket = get_server_socket(atoi(argv[2]));
+  int* csock; csock = malloc(NBVILLES*sizeof(int));
+  for (j = 0; j < NBVILLES; j++) {
+    csock[j] = wait_for_client(socket);
+    fprintf(stdout,"="); fflush(stdout);
+  }
+  printf("> Effectués\n");
 
-  pthread_join(serv,NULL);
-  for (j = 0; j < NBVILLES; j++) pthread_join(pt[j],NULL);
 
-  return(0);
+  // Démarrage
+  printf("Démarrage\n|");
+  int* corr; corr = malloc(N*sizeof(int)); // corresponance socket/machine
+  for (j = 0; j < NBVILLES; j++) send_string(csock[j],"start");
+  while (b) {
+
+    // wait for input...
+    FD_ZERO(&readfds);
+    for (j = 0; j < NBVILLES; j++) FD_SET(csock[j],&readfds); // from client
+    FD_SET(0,&readfds);		// .. from console
+    //FD_SET(socket,&readfds);	// .. another connection
+    if (select(MAXFILES, &readfds, NULL, NULL, NULL) <0) continue;
+
+    // check where input arrived
+    for (j = 0; j < NBVILLES; j++) {
+      if (FD_ISSET(csock[j],&readfds)) {
+        receive_packet(csock[j],(void**)&msg);
+        //printf("Message reçu par %d : %s\n",j,msg);
+        resp[j] = 1;
+        corr[j] = atoi(msg);
+        fprintf(stdout,"="); fflush(stdout);
+        //free(msg);
+      };
+    }
+    if (all_true(resp,NBVILLES)) b = 0;
+  }
+  printf("> Effectué\n");
+
+  init_0(resp,NBVILLES);
+  for (j = 0; j < NBVILLES; j++) send_string(csock[j],"beginning");
+
+
+  // Simulation
+  printf("Simulation\n");
+  int** immigr; immigr = malloc(NBVILLES*sizeof(int*));
+  for (i = 0; i < NBVILLES; i++) immigr[i] = malloc(N*sizeof(int));
+  b = 1;
+  for (i = 0; i < n; i++) {
+    b = 1;
+    //printf("*** Génération %d ***\n",i);
+    while (b) {
+
+      // wait for input...
+      FD_ZERO(&readfds);
+      for (j = 0; j < NBVILLES; j++) FD_SET(csock[j],&readfds); // from client
+      FD_SET(0,&readfds);		// .. from console
+      //FD_SET(socket,&readfds);	// .. another connection
+      if (select(MAXFILES, &readfds, NULL, NULL, NULL) <0) continue;
+
+      // check where input arrived
+      for (j = 0; j < NBVILLES; j++) {
+        if (FD_ISSET(csock[j],&readfds)) {
+          receive_packet(csock[j],(void**)&msg);
+          //printf("Message reçu par %d : %s\n",j,msg);
+          resp[j] = 1;
+          get_pop(msg,glb_pop[j]);
+          //free(msg);
+        };
+      }
+      if (all_true(resp,NBVILLES)) {
+        //printf("Migrations\n");
+        printf("\n");
+
+        migration(glb_pop,immigr);
+
+        print_server(i,corr,glb_pop,immigr);
+
+        for (j = 0; j < NBVILLES; j++) {
+          //printf("Envoi à %d\n",j);
+          msg = give_pop(immigr[j]);
+          send_string(csock[j],msg);
+          //free(msg);
+        }
+        b = 0;
+        init_0(resp,NBVILLES);
+      }
+    }
+  }
 }
